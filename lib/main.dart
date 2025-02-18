d3d9421aae94a5704905e930f26ef0ba0da65d99
hasanrabbi18 <rabbi15-4890@diu.edu.bd>
2025-02-18 10:31:41 +0600

    icon Used

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const labClass05());
+  runApp(const LabClass05());
 }
 
-class labClass05 extends StatelessWidget {
-  const labClass05({super.key});
+class LabClass05 extends StatelessWidget {
+  const LabClass05({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic Flutter UI-02',
+      title: "Basic Flutter UI-02",
       home: Scaffold(
         appBar: AppBar(
           title: Text("Basic Flutter UI-02"),
-          backgroundColor: const Color.fromARGB(255, 255, 234, 0),
+          backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Row(children: [
-          Text(
-            "Hi",
-            style: TextStyle(
-                color: const Color.fromARGB(255, 200, 15, 144), fontSize: 30),
-          ),
-          Text(
-            "This is",
-            style: TextStyle(
-                color: const Color.fromARGB(255, 15, 200, 33), fontSize: 50),
-          ),
-          Text(
-            "Hasan",
-            style: TextStyle(
-                color: const Color.fromARGB(255, 22, 1, 16), fontSize: 20),
-          ),
-        ]),
+        body: Row(
+          children: [
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star,
+             size: 50, 
+            ),
+            Icon(
+             Icons.star_outline,
+             size: 50, 
+            ),
+          ],
+        ),
       ),
     );
   }
